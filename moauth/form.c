9d971675d6ef2d516fc3731b1870bf8680b3bd5a
Michael Sweet <michael.r.sweet@gmail.com>
2017-11-15 07:55:53 -0500

    Implement form decoder and unit test.

--- moauth/form.c
+++ moauth/form.c
@@ -8,6 +8,14 @@
 
 #include <config.h>
 #include "moauth.h"
+#include <ctype.h>
+
+
+/*
+ * Local functions...
+ */
+
+static const char *decode_string(const char *data, char term, char *buffer, size_t bufsize);
 
 
 /*
@@ -18,7 +26,60 @@ int					/* O - Number of form variables or 0 on error */
 moauthFormDecode(const char    *data,	/* I - Form data */
                  cups_option_t **vars)	/* O - Form variables or @code NULL@ on error */
 {
-  (void)data;
+  int   num_vars = 0;                   /* Number of form variables */
+  char  ch,                             /* Current character */
+        name[1024],                     /* Variable name */
+        value[4096],                    /* Variable value */
+        *ptr;                           /* Pointer into name/value */
+
+
+ /*
+  * Scan the string for "name=value" pairs, unescaping values as needed.
+  */
+
+  *vars = NULL;
+
+  while (*data)
+  {
+   /*
+    * Get the name and value...
+    */
+
+    data = decode_string(data, '=', name, sizeof(name));
+
+    if (*data != '=')
+      goto decode_error;
+
+    data ++;
+
+    data = decode_string(data, '&', value, sizeof(value));
+
+    if (*data && *data != '&')
+      goto decode_error;
+    else if (*data)
+    {
+      data ++;
+
+      if (!*data)
+        goto decode_error;
+    }
+
+   /*
+    * Add the variable...
+    */
+
+    num_vars = cupsAddOption(name, value, num_vars, vars);
+  }
+
+  return (num_vars);
+
+ /*
+  * If we get here there was an error in the form data...
+  */
+
+  decode_error:
+
+  cupsFreeOptions(num_vars, *vars);
 
   *vars = NULL;
 
@@ -40,4 +101,63 @@ moauthFormEncode(int           num_vars,/* I - Number of form variables */
   (void)vars;
 
   return (NULL);
+}
+
+
+/*
+ * 'decode_string()' - Decode a URL-encoded string.
+ */
+
+static const char *                     /* O - New pointer into string */
+decode_string(const char *data,         /* I - Pointer into data string */
+              char       term,          /* I - Terminating character */
+              char       *buffer,       /* I - String buffer */
+              size_t     bufsize)       /* I - Size of string buffer */
+{
+  char  ch,                             /* Current character */
+        *ptr,                           /* Pointer info buffer */
+        *end;                           /* Pointer to end of buffer */
+
+
+  for (ptr = buffer, end = buffer + bufsize - 1; *data && *data != term; data ++)
+  {
+    if ((ch = *data) == '+')
+    {
+     /*
+      * "+" is an escaped space...
+      */
+
+      ch = ' ';
+    }
+    else if (ch == '%')
+    {
+     /*
+      * "%HH" is a hex-escaped character...
+      */
+
+      if (isxdigit(data[1] & 255) && isxdigit(data[2] & 255))
+      {
+        data ++;
+        if (isalpha(*data & 255))
+          ch = (tolower(*data & 255) - 'a' + 10) << 4;
+        else
+          ch = (*data - '0') << 4;
+
+        data ++;
+        if (isalpha(*data & 255))
+          ch += tolower(*data & 255) - 'a' + 10;
+        else
+          ch += *data - '0';
+      }
+      else
+        break;
+    }
+
+    if (ch && ptr < end)
+      *ptr++ = ch;
+  }
+
+  *ptr = '\0';
+
+  return (data);
 }
--- moauth/testmoauth.c
+++ moauth/testmoauth.c
@@ -7,6 +7,8 @@
  */
 
 #include <config.h>
+#include <stdio.h>
+#include <string.h>
 #include "moauth.h"
 
 
@@ -17,5 +19,82 @@
 int					/* O - Exit status */
 main(void)
 {
-  return (0);
+  int           status = 0;             /* Exit status */
+  int           i;                      /* Looping var */
+//  char          *data;                  /* Data pointer */
+  int           count,                  /* Expected variable count */
+                num_vars;               /* Number of variables */
+  cups_option_t *vars;                  /* Variables */
+  const char    *value;                 /* Value */
+  static const char * const decodes[] = /* Decode string tests */
+  {
+    "0?",
+    "0?name",
+    "0?%00=",
+    "1?empty=",
+    "1?name=value",
+    "0?name=value&",
+    "2?name1=value1&name2=value2",
+    "1?name+with+spaces=value+with+spaces",
+    "1?quotes=%22value%22"
+  };
+
+
+ /*
+  * Test different URL strings...
+  */
+
+  for (i = 0; i < (int)(sizeof(decodes) / sizeof(decodes[0])); i ++)
+  {
+    printf("moauthFormDecode(\"%s\", ...): ", decodes[i]);
+
+    count    = (int)strtol(decodes[i], NULL, 10);
+    num_vars = moauthFormDecode(decodes[i] + 2, &vars);
+
+    if (count != num_vars)
+    {
+      printf("FAIL (got %d variables, expected %d)\n", num_vars, count);
+      status = 1;
+    }
+    else if ((value = cupsGetOption("empty", num_vars, vars)) != NULL && strcmp(value, ""))
+    {
+      printf("FAIL (got \"empty=%s\", expected \"empty=\")\n", value);
+      status = 1;
+    }
+    else if ((value = cupsGetOption("name", num_vars, vars)) != NULL && strcmp(value, "value"))
+    {
+      printf("FAIL (got \"name=%s\", expected \"name=value\")\n", value);
+      status = 1;
+    }
+    else if ((value = cupsGetOption("name1", num_vars, vars)) != NULL && strcmp(value, "value1"))
+    {
+      printf("FAIL (got \"name1=%s\", expected \"name1=value1\")\n", value);
+      status = 1;
+    }
+    else if ((value = cupsGetOption("name2", num_vars, vars)) != NULL && strcmp(value, "value2"))
+    {
+      printf("FAIL (got \"name2=%s\", expected \"name2=value2\")\n", value);
+      status = 1;
+    }
+    else if ((value = cupsGetOption("name with spaces", num_vars, vars)) != NULL && strcmp(value, "value with spaces"))
+    {
+      printf("FAIL (got \"name with spaces=%s\", expected \"name with spaces=value with spaces\")\n", value);
+      status = 1;
+    }
+    else if ((value = cupsGetOption("quotes", num_vars, vars)) != NULL && strcmp(value, "\"value\""))
+    {
+      printf("FAIL (got \"quotes=%s\", expected \"quotes=\\\"value\\\"\")\n", value);
+      status = 1;
+    }
+    else
+      puts("PASS");
+
+    cupsFreeOptions(num_vars, vars);
+  }
+
+ /*
+  * Return the test results...
+  */
+
+  return (status);
 }
